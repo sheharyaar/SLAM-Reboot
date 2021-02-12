fc03edd456e126027ce36068edc913670d1107d3
Mohammad Shehar Yaar Tausif <34273345+sheharyaar@users.noreply.github.com>
2021-02-12 09:15:01 +0530

    Updated the repo

---
+++ SLAM/YTImport.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class YTImport
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ImportButton = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.DownloadWorker = New System.ComponentModel.BackgroundWorker()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripProgressBar1 = New System.Windows.Forms.ToolStripProgressBar()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ImportButton
+        '
+        Me.ImportButton.Location = New System.Drawing.Point(528, 12)
+        Me.ImportButton.Margin = New System.Windows.Forms.Padding(4)
+        Me.ImportButton.Name = "ImportButton"
+        Me.ImportButton.Size = New System.Drawing.Size(100, 28)
+        Me.ImportButton.TabIndex = 0
+        Me.ImportButton.Text = "Import"
+        Me.ImportButton.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(16, 15)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(503, 22)
+        Me.TextBox1.TabIndex = 4
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'DownloadWorker
+        '
+        Me.DownloadWorker.WorkerReportsProgress = True
+        Me.DownloadWorker.WorkerSupportsCancellation = True
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(383, 20)
+        Me.ToolStripStatusLabel1.Spring = True
+        Me.ToolStripStatusLabel1.Text = "Status: Idle"
+        Me.ToolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'ToolStripProgressBar1
+        '
+        Me.ToolStripProgressBar1.Name = "ToolStripProgressBar1"
+        Me.ToolStripProgressBar1.Size = New System.Drawing.Size(200, 18)
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.ToolStripProgressBar1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 53)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Padding = New System.Windows.Forms.Padding(1, 0, 19, 0)
+        Me.StatusStrip1.Size = New System.Drawing.Size(644, 26)
+        Me.StatusStrip1.SizingGrip = False
+        Me.StatusStrip1.TabIndex = 6
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'YTImport
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(644, 79)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ImportButton)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "YTImport"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "YouTube Import"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ImportButton As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents DownloadWorker As System.ComponentModel.BackgroundWorker
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents ToolStripProgressBar1 As ToolStripProgressBar
+    Friend WithEvents StatusStrip1 As StatusStrip
+End Class
---
+++ SLAM/YTImport.vb
@@ -0,0 +1,110 @@
+﻿Imports System.ComponentModel
+Imports System.IO
+Imports System.Diagnostics
+Imports System.Text.RegularExpressions
+Imports YoutubeExtractor
+Public Class YTImport
+
+    Public file As String
+    Sub ProgressChangedHandler(sender, args)
+        Console.WriteLine(args.ProgressPercentage)
+    End Sub
+
+    Private Sub ImportButton_Click(sender As Object, e As EventArgs) Handles ImportButton.Click
+        Dim youtubeMatch As Match = New Regex("youtu(?:\.be|be\.com)/(?:.*v(?:/|=)|(?:.*/)?)([a-zA-Z0-9-_]+)").Match(TextBox1.Text)
+        '////This code sends link to the downloader..bass
+        If youtubeMatch.Success Then
+            TextBox1.Enabled = False
+            ImportButton.Enabled = False
+            DownloadWorker.RunWorkerAsync("youtube.com/watch?v=" & youtubeMatch.Groups(1).Value)
+            ToolStripStatusLabel1.Text = "Status: Downloading"
+        Else
+            MessageBox.Show("Invalid YouTube URL.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            TextBox1.Enabled = True
+            ImportButton.Enabled = True
+        End If
+    End Sub
+
+    Private Sub DownloadWorker_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles DownloadWorker.DoWork
+        Try
+            'Dim videoInfos As IEnumerable(Of VideoInfo) = DownloadUrlResolver.GetDownloadUrls(e.Argument).OrderBy(Function(vid) vid.Resolution)
+
+            '////To understand this part-----------------------
+            'Dim video As VideoInfo = videoInfos.First(Function(info) info.AdaptiveType = AdaptiveType.Audio AndAlso info.AudioType = AudioType.Aac OrElse info.AdaptiveType = AdaptiveType.None AndAlso info.VideoType = VideoType.Mp4 AndAlso info.AudioBitrate >= 128)
+            'If IsNothing(video) Then
+            '    If videoInfos.Any(Function(info) info.AdaptiveType = AdaptiveType.None AndAlso info.VideoType = VideoType.Mp4) Then
+            '        video = videoInfos.First(Function(info) info.AdaptiveType = AdaptiveType.None AndAlso info.VideoType = VideoType.Mp4)
+            '    Else
+            '        Throw New System.Exception("Could not find download.")
+            '    End If
+            'End If
+
+            'If video.RequiresDecryption Then
+            '    DownloadUrlResolver.DecryptDownloadUrl(video)
+            'End If
+
+            If Not Directory.Exists(Path.GetFullPath("lagnos\")) Then
+                Directory.CreateDirectory(Path.GetFullPath("lagnos\"))
+            End If
+
+            Dim filename As String = ""
+
+            Dim proc As Process = New Process()
+            Dim name As String
+            name = Path.GetFullPath("youtube-dl.exe")
+            proc.StartInfo.FileName = name
+            proc.StartInfo.Arguments = e.Argument & " -o lagnos/%(title)s.%(ext)s --format mp4"
+            proc.StartInfo.UseShellExecute = False
+            proc.StartInfo.RedirectStandardOutput = True
+            proc.StartInfo.CreateNoWindow = True
+            proc.Start()
+            proc.WaitForExit()
+
+            Dim proc2 As Process = New Process()
+            Dim name2 As String
+            name2 = Path.GetFullPath("youtube-dl.exe")
+            proc2.StartInfo.FileName = name2
+            proc2.StartInfo.Arguments = e.Argument & " -e"
+            proc2.StartInfo.UseShellExecute = False
+            proc2.StartInfo.RedirectStandardOutput = True
+            proc2.StartInfo.CreateNoWindow = True
+            proc2.Start()
+            While proc2.StandardOutput.EndOfStream = False
+                filename = proc2.StandardOutput.ReadLine()
+            End While
+            proc2.WaitForExit()
+            e.Result = Path.GetFullPath("lagnos\" & filename & ".mp4")
+            '----------------------------------------
+
+        Catch ex As Exception
+            Form1.LogError(ex)
+            e.Result = ex
+        End Try
+    End Sub
+
+    Private Sub DownloadWorker_ProgressChanged(sender As Object, e As ProgressChangedEventArgs) Handles DownloadWorker.ProgressChanged
+        ToolStripProgressBar1.Value = e.ProgressPercentage
+    End Sub
+
+    Private Sub DownloadWorker_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs) Handles DownloadWorker.RunWorkerCompleted
+        If TypeOf e.Result Is Exception Then
+            MessageBox.Show(e.Result.Message & " See errorlog.txt for more info.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            'this is the file to be sent............................................................
+            file = e.Result
+            DialogResult = Windows.Forms.DialogResult.OK
+        End If
+    End Sub
+
+    Private Sub YTImport_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Select()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub DonateLabel_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
